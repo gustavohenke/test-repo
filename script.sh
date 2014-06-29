afcd5c1efa7a256e1403005384763f584b443868
Gustavo Henke <gustavo@injoin.com.br>
2014-06-29 18:53:05 -0300

    Update scripts, travis

--- script.sh
+++ script.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-if [ ! -z "${TRAVIS_TAG}" ]; then
-	echo "Tag available!"
-fi
+rm -rf ~/.ssh
+mkdir ~/.ssh
+echo -n $id_rsa_{00..30} >> ~/.ssh/id_rsa_base64
+base64 --decode --ignore-garbage ~/.ssh/id_rsa_base64 > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-echo $TRAVIS_TAG;
+GIT_NAME=`git show --format="%an" master | head -n 1`
+GIT_MAIL=`git show --format="%ae" master | head -n 1`
+
+# Configure git
+git config user.name "${GIT_NAME}"
+git config user.email "${GIT_MAIL}"
+
+echo foo > foo.bar
+git commit -am "Automated commit"
+git push origin master
